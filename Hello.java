8784def3f41a453943a8a8a359de126b39f8e6d8
jawad.shani@hitachi-eu.com <jawad.shani@hitachi-eu.com>
2020-03-09 08:20:05 +0100

    helloworld

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 
 public class Hello {
 
-	public static void main(string[] args){
+	public static void main(String args[]){
 
-		system.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 
 }
