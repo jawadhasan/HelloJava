1307c90767a5b3f51c53ac96a348f46fe57b47e5
jawad.shani@hitachi-eu.com <jawad.shani@hitachi-eu.com>
2020-11-23 08:58:13 +0100

    title-case converter example added

--- Hello.java
+++ Hello.java
@@ -4,6 +4,9 @@ public class Hello {
 	public static void main(String args[]){
 
 		System.out.println("Hello World");
+
+		var input = "hello to the world!";
+        System.out.println(TitleCaseConverter.convertToTitleCase(input));
 	}
 
 }
